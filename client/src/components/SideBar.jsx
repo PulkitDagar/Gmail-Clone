76636a42d914d06c85fcfcf16efdf75982246b30
Pulkit Dagar <dagarpulkit142@gmail.com>
2024-01-07 18:04:13 +0530

    update

--- client/src/components/SideBar.jsx
+++ client/src/components/SideBar.jsx
@@ -1,10 +1,28 @@
 
+import { Drawer, styled } from '@mui/material';
 
 const SideBar = () => {
-    return(
-        <div>
-            hello from sidebar
-        </div>
+    return (
+        <Drawer anchor='left' 
+            open={true} 
+            hideBackdrop={true} 
+            ModalProps={{
+                keepMounted: true
+            }}
+            variant='persistent'
+            sx={{
+                '& .MuiDrawer-paper': {
+                    marginTop: '64px',
+                    width: 250,
+                    background: '#F5F5F5',
+                    borderRight: 'none',
+                    height: 'calc(100vh-64px)'
+                }
+            }}
+        >
+        
+        </Drawer>
+
 
     )
 };
