873469eaeef7d1a4c686ad15d131ee02d023d132
Pulkit Dagar <dagarpulkit142@gmail.com>
2024-01-09 17:32:33 +0530

    update

---
+++ client/src/services/api.js
@@ -0,0 +1,12 @@
+import axios from 'axios';
+
+const API_URL = 'https://localhost:8000';
+const API_GMAIL = async()=> {
+    return  await axios({
+        method: 'delete',
+        url: `${API_URL}/endpoint`,
+        data: {}
+    })
+}
+
+export default API_GMAIL
