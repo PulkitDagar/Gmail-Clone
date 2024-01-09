cdf6cecf9b144bd8401eca4c291e14d6c27dc1d4
Pulkit Dagar <dagarpulkit142@gmail.com>
2024-01-09 17:19:07 +0530

    updates

--- server/database/db.js
+++ server/database/db.js
@@ -1,8 +1,13 @@
 import mongoose from "mongoose";
 const Connection = () => {
+
+    const DB_URI = `mongodb://pulkitd132:EH1pANhpxLszSLhT@ac-gsjqnfh-shard-00-00.5spvmy6.mongodb.net:27017,ac-gsjqnfh-shard-00-01.5spvmy6.mongodb.net:27017,ac-gsjqnfh-shard-00-02.5spvmy6.mongodb.net:27017/?ssl=true&replicaSet=atlas-o2tp67-shard-0&authSource=admin&retryWrites=true&w=majority`
     try{
-        mongoose.connect()
+        mongoose.connect(DB_URI, {useNewUrlParser: true});
+        console.log('database connected successfully');
     } catch(error){
         console.log('Error while connecting with the database', error.message);
     }
 }
+
+export default Connection;
